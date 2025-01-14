41f65f26e5cbd9ab1c64cdc70e3160fc3d666f49
gulshan147 <dfjgdjmh@gmail.com>
2025-01-14 19:14:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY Gulshan Alam
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7640575553:AAH8qlaGe6lCly25k8O2pnNDTfIStZpPMkg")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Gulshan_Alam")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6189146935")
